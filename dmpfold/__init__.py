a948f62232a87123d39f9043b04b04c1f06259bd
Joe Greener <jgreener@hotmail.co.uk>
2020-09-07 19:40:33 +0100

    binary outline

---
+++ dmpfold/__init__.py
@@ -0,0 +1,1 @@
+from .protocols import protocol_fdf, protocol_cns, protocol_xplor
---
+++ dmpfold/protocols.py
@@ -0,0 +1,8 @@
+def protocol_fdf():
+    print("test fdf")
+
+def protocol_cns():
+    print("CNS not implemented yet")
+
+def protocol_xplor():
+    print("Xplor-NIH not implemented yet")
