832f049fc6352555eedd74d2886be059e96a5454
Joe Greener <jgreener@hotmail.co.uk>
2020-09-28 18:33:32 +0100

    cns protocol shell scripts

--- dmpfold/cns.py
+++ dmpfold/cns.py
@@ -1,2 +1,21 @@
-def aln_to_model_cns():
-    print("CNS not implemented yet")
+import os
+from subprocess import run
+
+def aln_to_model_cns(aln_filepath, out_dir):
+    with open(aln_filepath, "r") as f:
+        aln = f.read().splitlines()
+    sequence = aln[0]
+    length = len(sequence)
+    print("Sequence has", length, "residues:")
+    print(sequence)
+
+    with open("input.fasta", "w") as f:
+        f.write(">SEQ\n")
+        f.write(sequence + "\n")
+    bin_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), "bin")
+    run(f"{bin_dir}/fasta2tlc < input.fasta > input.seq", shell=True)
+
+    cns_cmd = "cns"
+    cnsfile_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), "cnsfiles")
+    run(f"{cns_cmd} < {cnsfile_dir}/gseq.inp > gseq.log", shell=True)
+    run(f"{cns_cmd} < {cnsfile_dir}/extn.inp > extn.log", shell=True)
