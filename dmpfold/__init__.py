d2f7347d256eb0c983a4390a3284830f1c662e5c
Joe Greener <jgreener@hotmail.co.uk>
2021-07-13 14:01:38 +0100

    delete folding framework

--- dmpfold/__init__.py
+++
@@ -1,4 +0,0 @@
-from .fdf import aln_to_model_fdf, ForceFolder
-from .cns import aln_to_model_cns
-from .xplor import aln_to_model_xplor
-from .networks import aln_to_predictions, aln_to_predictions_iter, write_predictions
